17c7d7d970550aec365d331015ed963b5abd62f6
derekb63 <derekrbean@gmail.com>
2017-01-17 09:05:19 -0800

    minor changes for debugging

--- lab1/close.py
+++ lab1/close.py
@@ -1,10 +1,8 @@
 #!/usr.bin/env/python
 
 def is_close(a, b, c):
-    if a-b < c:
-        return True
-    else:
-        return False
+	if abs(a-b) < c:
+		return True
+	else:
+		return False
 
-
-if __name__ == '__main__':
