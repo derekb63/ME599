b0411232c64f5c6f9d682cf8d8e30fed051e65b8
derekb63 <beande@oregonstate.edu>
2017-01-25 22:41:44 -0800

    Finished the iterative and recursive functions for reversing a list along with checks for correct reversal and inputting incorrect lists.

--- lab2/reverse.py
+++ lab2/reverse.py
@@ -1,4 +1,4 @@
-!# usr/bin/env python
+#! usr/bin/env python
 
 # Derek Bean
 # ME599
@@ -9,26 +9,50 @@
 
 
 #Iterative list reverse function
-def reverse_i(list):
-	reversed_list = []
-	return reversed_list
+def reverse_i(list_in):
+        if type(list_in) != list or not(all(isinstance(i, (int, float)) for i in list_in)):
+                print('The input variable was not a list of ints or floats.')
+                return None
+        reversed_list = []
+        for i in range(len(list_in)):
+                reversed_list.append(list_in[-(i+1)])
+        return reversed_list
 
 
 #Recursive list reverse function
-def reverse_r(list):
-	reversed_list = []
-	return reversed_list
+def reverse_r(list_in):
+        if type(list_in) != list or not(all(isinstance(i, (int, float)) for i in list_in)):
+                print('The input variable was not a list of ints or floats.')
+                return None
+        reverse_list=[]
+        if len(list_in) == 1:
+                reverse_list += list_in
+                return reverse_list
+        else:
+                reverse_list = [list_in[-1]] + reverse_r(list_in[:-1])
+        return reverse_list
+ 
 
-
-
-if __name__ == '__main__'
-	example_list = [1, 2, 3, 4, 5, 6]
-	if reverse_i(example_list) != example_list.reverse():
-		print 'The function reverse_i has returned an incorrent value'
-	else:
-		print 'The function reverse_i returned the correct value'
-		
-	if reverse_r(example_list) != example_list.reverse():
-		print 'The function reverse_r has returned an incorrent value'
-	else:
-		print 'The function reverse_r returned the correct value'
+if __name__ == '__main__':
+        correct_list = [1, 2, 3, 4, 5, 6]
+        reversed_correct_list = correct_list[:]
+        reversed_correct_list.reverse()
+        if reverse_i(correct_list) == reversed_correct_list:
+                print('The function reverse_i returned the correct value')
+        else:
+                print('The function reverse_i has returned an incorrect value')
+                
+        if reverse_r(correct_list) == reversed_correct_list:
+                print('The function reverse_r has returned the correct value')
+        else:
+                print('The function reverse_r returned an incorrect value')
+                
+        incorrect_list = ['a', [1, 2], 3, (4, 5)]
+        if reverse_i(incorrect_list) != None:
+                print('The fuction reverse_i did not identify an incorrect input')
+        else:
+                print('reverse_i identified an incorrect input')
+        if reverse_r(incorrect_list) != None:
+                print('The fuction reverse_r did not identify an incorrect input')
+        else:
+                print('reverse_r identified an incorrect input')
