522def0dccc045a620f965ff55be0c381b116692
derekb63 <beande@oregonstate.edu>
2017-01-25 20:58:59 -0800

    finished the recursion sum calculator. Added the identifiers to handle incorrect inputs and the appropriate test cases

--- lab2/sum.py
+++ lab2/sum.py
@@ -6,28 +6,52 @@
 
 
 # Find the sum of a list of numbers using a for loop
-def sum_i(list):
-	list_sum = 0
-	for i in list:
-		list_sum += i
-	return list_sum
-	
+def sum_i(list_in):
+        if type(list_in) != list or not(all(isinstance(i, (int, float)) for i in list_in)):
+                print('The input variable was not a list of ints or floats.')
+                return None
+
+        list_sum = 0
+        for i in list_in:
+                list_sum += i
+        return list_sum
+        
 
 # Caclulate the sum of a list of numbers using recursion
-def sum_r(list):
-	list_sum = 0
-	
-	return list_sum
-	
-	
+def sum_r(list_in):
+        if type(list_in) != list or not(all(isinstance(i, (int, float)) for i in list_in)):
+                print('The input variable was not a list of ints or floats.')
+                return None
+
+        list_sum = 0
+        if len(list_in) == 1:
+                list_sum = list_in[0]
+                return list_sum
+        else:
+                list_sum = list_in[0] + sum_r(list_in[1:])
+        return list_sum
+        
+        
 if __name__ == '__main__':
-	example_list = [1, 2, 3, 4, 5, 6]
-	if sum_i(example_list) != sum(example_list):
-		print 'The function sum_i has returned an incorrent value'
-	else:
-		print 'sum_i returned the correct value'
-		
-	if sum_r(example_list) != sum(example_list):
-		print 'The function sum_r has returned an incorrent value'
-	else:
-		print 'sum_r returned the correct value'
+        
+        correct_list = [1, 2, 3, 4, 5, 6]
+        if sum_i(correct_list) != sum(correct_list):
+                print 'The function sum_i has returned an incorrent value'
+        else:
+                print 'sum_i returned the correct value'
+
+        if sum_r(correct_list) != sum(correct_list):
+                print 'The function sum_r has returned an incorrent value'
+        else:
+                print 'sum_r returned the correct value'
+
+        incorrect_list = ['a', [1, 2], 3, (4, 5)]
+        if sum_i(incorrect_list) != None:
+                print('The fuction sum_i did not identify an incorrect input')
+        else:
+                print('sum_i identified an incorrect input')
+        if sum_i(incorrect_list) != None:
+                print('The fuction sum_r did not identify an incorrect input')
+        else:
+                print('sum_r identified an incorrect input')
+        
