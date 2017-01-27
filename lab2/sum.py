0f80a0a968026cf4697bc86872fa81fb103b26c9
derekb63 <beande@oregonstate.edu>
2017-01-26 20:27:25 -0800

    Formatted to pep8 and put status file in lab2 folder

--- lab2/sum.py
+++ lab2/sum.py
@@ -1,13 +1,14 @@
 #! usr/bin/env python
 
 # Derek Bean
-# ME 599 
+# ME 599
 # 1/24/2017
 
 
 # Find the sum of a list of numbers using a for loop
 def sum_i(list_in):
-        if type(list_in) != list or not(all(isinstance(i, (int, float)) for i in list_in)):
+        if type(list_in) != list or not(all(isinstance(i, (int, float))
+                                        for i in list_in)):
                 print('The input variable was not a list of ints or floats.')
                 return None
 
@@ -15,11 +16,12 @@ def sum_i(list_in):
         for i in list_in:
                 list_sum += i
         return list_sum
-        
+
 
 # Caclulate the sum of a list of numbers using recursion
 def sum_r(list_in):
-        if type(list_in) != list or not(all(isinstance(i, (int, float)) for i in list_in)):
+        if type(list_in) != list or not(all(isinstance(i, (int, float))
+                                        for i in list_in)):
                 print('The input variable was not a list of ints or floats.')
                 return None
 
@@ -30,10 +32,10 @@ def sum_r(list_in):
         else:
                 list_sum = list_in[0] + sum_r(list_in[1:])
         return list_sum
-        
-        
+
+
 if __name__ == '__main__':
-        
+
         correct_list = [1, 2, 3, 4, 5, 6]
         if sum_i(correct_list) != sum(correct_list):
                 print 'The function sum_i has returned an incorrent value'
@@ -46,12 +48,11 @@ if __name__ == '__main__':
                 print 'sum_r returned the correct value'
 
         incorrect_list = ['a', [1, 2], 3, (4, 5)]
-        if sum_i(incorrect_list) != None:
+        if sum_i(incorrect_list) is not None:
                 print('The fuction sum_i did not identify an incorrect input')
         else:
                 print('sum_i identified an incorrect input')
-        if sum_r(incorrect_list) != None:
+        if sum_r(incorrect_list) is not None:
                 print('The fuction sum_r did not identify an incorrect input')
         else:
                 print('sum_r identified an incorrect input')
-        
