c1f0b215540eb6c263b67933a365e2aa89435161
derekb63 <beande@oregonstate.edu>
2017-01-28 12:35:13 -0800

    Completed the pi estimate function with an error of 7.642e-8 when compared to math.pi. Better accuracy could be obtained by forcing more iterations

--- lab2/estimate_pi.py
+++ lab2/estimate_pi.py
@@ -8,14 +8,29 @@ import math
 import numpy as np
 
 
+# Return the value of the sum for the input value k
+def sum_term(k):
+    return np.divide((math.factorial(4*k))*(1103+26390*k)*np.power(396, -4*k),
+                     np.power(math.factorial(k), 4))
+
+
 def estimate_pi():
-    k = 0
+    # Cacluate the constant in front of the summation term
     constant = np.divide(2*np.sqrt(2), 9801)
-    while summation > 1e-15:
-        summation = np.divide((math.factorial(4*k))*(1103+26390*k),
-                              np.power(math.factorial(k), 4)*np.power(396, 4*k))
+    # initialize the first summation term for a while loop initial comparator
+    k = 0
+    current_sum_term = sum_term(k)
+    summation = current_sum_term
+
+    while current_sum_term > 1e-15:
         k += 1
-    
+        current_sum_term = sum_term(k)
+        summation += current_sum_term
+
+    pi_estimate = np.divide(1, constant * summation)
     return pi_estimate
+
 if __name__ == '__main__':
-    print(estimate_pi())
+
+    print 'Pi est: ', estimate_pi()
+    print 'Error: ', estimate_pi()-math.pi
