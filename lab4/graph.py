3a02c166e78198564dbb85d70be1a957d3e4dccd
Derek Bean <derekrbean@gmail.com>
2017-02-13 14:26:44 -0800

    Added the instructions for the labs and homeworks to-date for future refernce

--- lab4/graph.py
+++ lab4/graph.py
@@ -13,16 +13,16 @@ from sorting_functions import *
 
 bubble_time = []
 sorted_time = []
-list_sizes = np.arange(100, 2000, 100) 
+list_sizes = np.arange(100, 2000, 100)
 for x in list_sizes:
-	test_list = list(np.random.randint(10, size=x))
-	# Test if a list is sorted
-	bubble_time.append(bubblesort(test_list)[1])
-	if is_sorted(bubblesort(test_list)[0]) is False:	
-		print 'bubblesort did not sort test list #{0}'.format(x)
-	sort_time_start = time()
-	sorted(test_list)
-	sorted_time.append(time()-sort_time_start)
+    test_list = list(np.random.randint(10, size=x))
+    # Test if a list is sorted
+    bubble_time.append(bubblesort(test_list)[1])
+    if is_sorted(bubblesort(test_list)[0]) is False:
+        print 'bubblesort did not sort test list #{0}'.format(x)
+    sort_time_start = time()
+    sorted(test_list)
+    sorted_time.append(time()-sort_time_start)
 plt.loglog(list_sizes, bubble_time)
 plt.loglog(list_sizes, sorted_time)
 plt.xlabel('List Length')
--- lab4/sorting_functions.py
+++ lab4/sorting_functions.py
@@ -9,30 +9,32 @@ from time import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Implement the bubblesort algorithm that takes a list of numbers and returns the sorted version of the list
+# Implement the bubblesort algorithm that takes a list of numbers and returns
+# the sorted version of the list
+
 
 def bubblesort(to_sort):
-	start = time()
-	for x in range(len(to_sort)):
-		for idx in range(len(to_sort)-1):
-			if to_sort[idx] > to_sort[idx+1]:
-				to_sort[idx+1], to_sort[idx] = to_sort[idx], to_sort[idx+1]
-	return to_sort, time()-start
+    start = time()
+    for x in range(len(to_sort)):
+        for idx in range(len(to_sort)-1):
+            if to_sort[idx] > to_sort[idx+1]:
+                to_sort[idx+1], to_sort[idx] = to_sort[idx], to_sort[idx+1]
+    return to_sort, time()-start
 
 
 def is_sorted(check_list):
-	if check_list == sorted(check_list):
-		return True
-	else:
-		return False
+    if check_list == sorted(check_list):
+        return True
+    else:
+        return False
 
 
 def quicksort(to_sort):
         if len(to_sort) == 1:
                 return to_sort
         pivot = to_sort[int(len(to_sort)/2)]
-        smaller = [ i for i in to_sort if i <= pivot]
-        larger = [ i for i in to_sort if i > pivot]
+        smaller = [i for i in to_sort if i <= pivot]
+        larger = [i for i in to_sort if i > pivot]
         print('To sort:', to_sort)
         print('Pivot:',   pivot)
         print('Smaller:', smaller)
@@ -42,17 +44,13 @@ def quicksort(to_sort):
 
 
 if __name__ == '__main__':
-	list_sizes = np.arange(100, 200, 100) 
+    list_sizes = np.arange(100, 200, 100)
 
-	for x in list_sizes:
-		test_list = list(np.random.randint(10, size=x))
-		# Test if a list is sorted
-		if is_sorted(bubblesort(test_list)[0]) is False:	
-			print 'bubblesort did not sort test list #{0}'.format(x)
-	test_list = list(np.random.randint(10, size=10))
-	print test_list
-	print quicksort(test_list)
-	
-
-
-
+    for x in list_sizes:
+        test_list = list(np.random.randint(10, size=x))
+        # Test if a list is sorted
+        if is_sorted(bubblesort(test_list)[0]) is False:
+            print 'bubblesort did not sort test list #{0}'.format(x)
+    test_list = list(np.random.randint(10, size=10))
+    print test_list
+    print quicksort(test_list)
