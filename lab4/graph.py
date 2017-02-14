377b12e4e263da135ba312be856eb3c699f836f3
Derek Bean <derekrbean@gmail.com>
2017-02-13 19:56:34 -0800

    finished quicksort and added insertion sort to the lab 4 sorting functions file

--- lab4/graph.py
+++ lab4/graph.py
@@ -8,25 +8,44 @@
 from time import time
 import numpy as np
 import matplotlib.pyplot as plt
-from sorting_functions import *
+from tqdm import tqdm
+from sorting_functions import bubblesort, quicksort, insertionsort
 
 
 bubble_time = []
 sorted_time = []
-list_sizes = np.arange(100, 2000, 100)
-for x in list_sizes:
+quicksort_time = []
+insert_time = []
+list_sizes = np.arange(1, 10000, 10)
+
+
+for x in tqdm(list_sizes):
     test_list = list(np.random.randint(10, size=x))
-    # Test if a list is sorted
-    bubble_time.append(bubblesort(test_list)[1])
-    if is_sorted(bubblesort(test_list)[0]) is False:
-        print 'bubblesort did not sort test list #{0}'.format(x)
+
+    bubble_time_start = time()
+    bubblesort(test_list)
+    bubble_time.append(time()-bubble_time_start)
+
     sort_time_start = time()
     sorted(test_list)
     sorted_time.append(time()-sort_time_start)
+
+    quicksort_time_start = time()
+    quicksort(test_list)
+    quicksort_time.append(time()-quicksort_time_start)
+    
+    insert_time_start = time()
+    insertionsort(test_list)
+    insert_time.append(time()-insert_time_start)
+    
 plt.loglog(list_sizes, bubble_time)
 plt.loglog(list_sizes, sorted_time)
+plt.loglog(list_sizes, quicksort_time)
+plt.loglog(list_sizes, insert_time)
+
 plt.xlabel('List Length')
 plt.ylabel('Sort Time (s)')
 plt.title('List Sort Time for Various Methods')
-plt.legend(['Bubble Sort', 'Sorted'])
+plt.legend(['Bubble Sort', 'Sorted', 'Quicksort', 'Insertion Sort'])
+
 plt.show()
--- lab4/sorting_functions.py
+++ lab4/sorting_functions.py
@@ -7,19 +7,18 @@
 
 from time import time
 import numpy as np
-import matplotlib.pyplot as plt
-
-# Implement the bubblesort algorithm that takes a list of numbers and returns
-# the sorted version of the list
+import random
 
 
 def bubblesort(to_sort):
-    start = time()
+    if is_sorted(to_sort) == True:
+        return to_sort
+        
     for x in range(len(to_sort)):
         for idx in range(len(to_sort)-1):
             if to_sort[idx] > to_sort[idx+1]:
                 to_sort[idx+1], to_sort[idx] = to_sort[idx], to_sort[idx+1]
-    return to_sort, time()-start
+    return to_sort
 
 
 def is_sorted(check_list):
@@ -30,27 +29,53 @@ def is_sorted(check_list):
 
 
 def quicksort(to_sort):
-        if len(to_sort) == 1:
-                return to_sort
-        pivot = to_sort[int(len(to_sort)/2)]
-        smaller = [i for i in to_sort if i <= pivot]
-        larger = [i for i in to_sort if i > pivot]
-        print('To sort:', to_sort)
-        print('Pivot:',   pivot)
-        print('Smaller:', smaller)
-        print('Larger:',  larger)
-        sorted_list = quicksort(smaller) + pivot + quicksort(larger)
+    if is_sorted(to_sort) == True:
+        return to_sort
+        
+    elif len(to_sort) == 2:
+        sorted_list = [min(to_sort), max(to_sort)]
         return sorted_list
+        
+    elif len(to_sort) == 3:
+        min_val = to_sort.pop(to_sort.index(min(to_sort)))
+        max_val = to_sort.pop(to_sort.index(max(to_sort)))
+        mid_val = to_sort
+        sorted_list = [min_val, mid_val, max_val]
+        return sorted_list
+        
+    pivot = random.choice(to_sort)
+    smaller = [i for i in to_sort if i < pivot]
+    larger = [i for i in to_sort if i >= pivot]
+    
+    sorted_list = quicksort(smaller) + quicksort(larger)
+    return sorted_list
 
-
+   
+def insertionsort(to_sort):
+    if is_sorted(to_sort) == True:
+        return to_sort
+        
+    for x in range(len(to_sort)+1):
+        key = to_sort[x-1]
+        j = x-2
+        while j >= 0 and to_sort[j] > key:
+            to_sort[j+1], to_sort[j] = to_sort[j], to_sort[j+1]
+            j = j-1
+        to_sort[j+1] = key
+    return to_sort
 if __name__ == '__main__':
-    list_sizes = np.arange(100, 200, 100)
+    # dgfasg
 
+    list_sizes = np.arange(10, 200, 10)
+    
     for x in list_sizes:
         test_list = list(np.random.randint(10, size=x))
-        # Test if a list is sorted
-        if is_sorted(bubblesort(test_list)[0]) is False:
-            print 'bubblesort did not sort test list #{0}'.format(x)
-    test_list = list(np.random.randint(10, size=10))
-    print test_list
-    print quicksort(test_list)
+    
+        if is_sorted(bubblesort(test_list[:])) is False:
+            print 'bubblesort did not sort test list of length {0}'.format(x)
+    
+        if is_sorted(quicksort(test_list[:])) is False:
+            print 'quicksort did not sort test list of length {0}'.format(x)
+    
+        if is_sorted(insertionsort(test_list[:])) is False:
+            print 'insertion did not sort test list of length {0}'.format(x)
