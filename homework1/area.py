4d32575d9ed43ad74d8e08df042cf7719927ee24
Derek Bean <derekrbean@gmail.com>
2017-02-27 10:58:13 -0800

    Fixed help comment

--- homework1/area.py
+++ homework1/area.py
@@ -5,6 +5,20 @@
 # Homework 1
 # 1/24/2017
 
+'''
+The function area usies Monte Carlo Integration to deterine the area of a shape
+Specifically, it uses rejection sampling to find the density
+
+    Inputs:
+        f: the function for which the area is calculated
+        p1: defines the forst corner of the bounding box around the area
+        p2: defines the second corner of the bounding box around the area
+        samples: sets how many samples to use for the integration
+
+    Outputs:
+        area: the area of the arbitrary shape input to the function
+'''
+
 from types import LambdaType
 import numpy as np
 import matplotlib.pyplot as plt
@@ -45,7 +59,7 @@ def area(f, p1, p2, samples=1e4):
         if f(*j) <= 0:
             count += 1
 
-    area = test_area*(float(count)/float(samples))
+    area = (test_area/float(samples))*count
     return area
 
 
@@ -59,8 +73,12 @@ if __name__ == '__main__':
     p2 = (2, 2)
 
     error = []
-    samples = np.linspace(10, 1e6, 15)
+    samples = np.linspace(10, 1e4, 100)
     for k in samples:
         a = area(circle, p1, p2, k)
-        error.append(abs(np.pi - a))
+        error.append(abs(np.pi - a)/np.pi)
     plt.loglog(samples, error)
+    plt.xlabel(r'Number of random samples')
+    plt.ylabel(r'Error')
+    plt.title(r'Error for Monte Carlo Estimation of $\pi$')
+    plt.show()
