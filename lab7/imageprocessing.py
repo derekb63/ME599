ca6a88f04a945406a00fcbd5108823533c437f8e
derekb63 <derekrbean@gmail.com>
2017-02-28 09:47:47 -0800

    Started lab 6. The ImProcess return the average pixel density for the selected image. Get image pulls the preset amount of images from the webcam and puts them in the directory images. It also makes the directory images if it does not exist.

---
+++ lab7/imageprocessing.py
@@ -0,0 +1,62 @@
+#! usr/bin/env python
+
+# Derek Bean
+# ME 599
+# lab 7
+# 2/28/2017
+
+from grabber import Webcam
+from PIL import Image, ImageChops
+import os
+
+# To save an image from grabber use the syntax
+# <variable name> = Webcam()
+# <variable name>.save_image(<filename>)
+
+# Tasks of the lab
+#     Calculate the average intensity of an image
+#     Plot the average intestity over time
+#     Write a code that returns the filtered average intensity
+#     write a function daytime that returns True and False
+#     write a function that grabs an image and calculates the most common
+#         color in it. what is the color, how many pixels have this color
+#     Write a function that detects mothion in the image stream that returns
+#         the average pixel difference in a function called motion that
+#         returns True or False if there is motion in the quad
+#     Write a function called event that returns true if there's an event
+#         happening in the quad
+
+# https://docs.python.org/3/library/sched.html
+# https://docs.python.org/2/library/os.html#os.listdir
+
+
+class ImProcess:
+    def __init__(self,image_file):
+        self.image= Image.open(image_file)
+
+    def show(self):
+        return self.image.show()
+
+    def avg_intensity(self):
+        pixel_data = list(self.image.getdata())
+        pixel_mean = [sum(x)/len(x) for x in pixel_data]
+	return  sum(pixel_mean) / len(pixel_mean)
+
+
+class GetImage:
+    def __init__(self, number_of_images, folder='images'):
+        for x in xrange(number_of_images):
+            image = Webcam()
+            try:
+                image.save_image(folder+'/'+'image{0}'.format(x))
+            except:
+                os.makedirs(folder)
+                image.save_image(folder+'/'+'image{0}'.format(x))
+
+if __name__ == '__main__':
+    # test = ImProcess('test-image')
+    # test.show()
+    # print test.avg_intensity()
+    GetImage(5)
+
+
