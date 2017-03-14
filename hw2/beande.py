47ed6e582ded456ed4112a52a047701de4cc89fa
derekb63 <derekrbean@gmail.com>
2017-03-14 09:52:27 -0700

    Little bit of work for homework 2 and  the submission file for lab7

--- hw2/beande.py
+++ hw2/beande.py
@@ -6,27 +6,29 @@ Created on Thu Mar  9 15:27:16 2017
 @author: aero-10
 """
 import random
+from collections import Counter
 
 
 class Beande:
     def __init__(self, name='beande'):
         self.name = name
         self.plays = {'Rock': 0, 'Paper': 1, 'Scissors': 2}
-        self.history = []
-        self.seed = 'Scissors'
+        self.opp_history = []
+        self.seed = 'Paper'
         random.seed(self.plays[self.seed])
 
     def play(self, name):
-        return random.randint(0, 2)
+	play = random.randint(0, 2)
+        return play
 
     def learn(self, opp_name, opp_move):
-        self.history.append(opp_move)
-        enough_games = len(self.history) == 10
-        if enough_games:
-            same_move = len(set(self.history)) == 1
-            if same_move:
-                same_move_as_seed = set(self.history).pop()
-                if same_move_as_seed:
+        self.opp_history.append(opp_move)
+        enough_games = len(self.opp_history)%10 == 0
+        if enough_games is True:
+            same_move = len(set(self.opp_history)) == 1
+            if same_move is True:
+                same_move_as_seed = set(self.opp_history).pop()
+                if same_move_as_seed is True:
                     random.seed(random.choice(self.plays.keys()))
 
 
